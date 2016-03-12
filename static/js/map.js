ca85fb2486b2d8fbe174125f1284f6ed559b1a78
dontpkme <dontpkme@gmail.com>
2016-03-13 05:14:18 +0800

    optimize map iamge

--- static/js/map.js
+++ static/js/map.js
@@ -1,30 +1,41 @@
 $(document).ready(function() {
+	var $token;
 	render();
+
+	$(".token").click(function(e) {
+		$token = $(e.target);
+		$(e.target).addClass("selected");
+	});
+
+	$(".map").click(function(e) {
+		if ($token != undefined) {
+			$token.removeClass("selected").animate({
+				"top": (e.pageY - 15) + "px",
+				"left": (e.pageX - 15) + "px",
+			}, function(){
+				$token = undefined;
+			});
+		}
+	});
 });
 
 var render = function() {
 	var ww = $(window).width();
 	var wh = $(window).height();
-	var $token;
 
 	if (ww > wh) {
 		$(".mask").show();
-		$(".token").removeClass("hide");
+		$(".token").removeClass("hide").css({
+			"width": (wh*0.05)+"px",
+			"height": (wh*0.05)+"px",
+		});
 	} else {
 		$(".mask").hide();
-	}
-
-	$(".token").click(function(e) {
-		$token = $(this);
-		$(this).addClass("selected");
-	});
-
-	$(".map").click(function(e) {
-		$token.removeClass("selected").animate({
-			"top": (e.pageY - 15) + "px",
-			"left": (e.pageX - 15) + "px",
+		$(".token").css({
+			"width": (ww*0.055)+"px",
+			"height": (ww*0.055)+"px",
 		});
-	});
+	}
 }
 
 $(window).resize(function() {
